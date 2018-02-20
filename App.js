186d605df19f99d50c5e6422036a55fe4ba21a01
Harish V <harishv207@gmail.com>
2018-02-20 23:22:07 +0800

    Basic Topic layout

--- App.js
+++ App.js
@@ -1,23 +1,49 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Alert, StyleSheet, Text, View, Image, Button } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+class Topic extends Component {
+    _onPressButton() {
+        Alert.alert('You tapped the button!');
+    }
+
+    render() {
+        return (
+            <View style={{flex:1, padding: 10}}>
+                <Text>{this.props.title}</Text>
+                <View style={{flexDirection: 'row'}}>
+                    <Button title='Upvote' onPress={this._onPressButton} />
+                    <Button title='Downvote' onPress={this._onPressButton} />
+                </View>
+            </View>
+        );
+    }
+}
+
+export default class Topics extends Component {
+    render() {
+        return ( 
+            <View style={{flex:1}}>
+                <Topic title='Welcome to my life.' />
+                <Topic title='Simple Plan.' />
+                <Topic title='Linkin Park.' />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    bigBlue: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 30
+    },
+    red: {
+        color: 'red'
+    }
 });
